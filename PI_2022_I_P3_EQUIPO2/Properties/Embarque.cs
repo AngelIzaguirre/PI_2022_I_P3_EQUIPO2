2905722b191269074e7edda5d6652a9cede6da6e
AngelIzaguirre <angel.izaguirre@ujcv.edu.hn>
2022-04-18 21:54:46 -0600

    nuevo

---
+++ PI_2022_I_P3_EQUIPO2/Properties/Embarque.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_2022_I_P3_EQUIPO2.Properties
+{
+    internal class Embarque
+    {
+    }
+}
