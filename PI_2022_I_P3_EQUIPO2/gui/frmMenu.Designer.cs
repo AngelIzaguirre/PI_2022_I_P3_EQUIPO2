6701f37e158d6e1f5b29d322ed8165d878ae43dd
AngelIzaguirre <angel.izaguirre@ujcv.edu.hn>
2022-04-18 21:45:49 -0600

    nuevo

--- PI_2022_I_P3_EQUIPO2/gui/frmMenu.Designer.cs
+++ PI_2022_I_P3_EQUIPO2/gui/frmMenu.Designer.cs
@@ -35,13 +35,7 @@
             this.btnEmpleado = new System.Windows.Forms.Button();
             this.btnEquipaje = new System.Windows.Forms.Button();
             this.btnEmbarque = new System.Windows.Forms.Button();
-            this.btnSeguridad = new System.Windows.Forms.Button();
-            this.btnLimpieza = new System.Windows.Forms.Button();
             this.lblMenu = new System.Windows.Forms.Label();
-            this.btnAerolinea = new System.Windows.Forms.Button();
-            this.btnCafeteria = new System.Windows.Forms.Button();
-            this.btnSoporteTecnico = new System.Windows.Forms.Button();
-            this.btnMigracion = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuPrincipalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -56,12 +50,11 @@
             this.btnPersona.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnPersona.Image = ((System.Drawing.Image)(resources.GetObject("btnPersona.Image")));
             this.btnPersona.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPersona.Location = new System.Drawing.Point(205, 221);
+            this.btnPersona.Location = new System.Drawing.Point(659, 201);
             this.btnPersona.Name = "btnPersona";
-            this.btnPersona.Size = new System.Drawing.Size(89, 29);
+            this.btnPersona.Size = new System.Drawing.Size(125, 33);
             this.btnPersona.TabIndex = 0;
             this.btnPersona.Text = "Persona";
-            this.btnPersona.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnPersona.UseVisualStyleBackColor = true;
             this.btnPersona.Click += new System.EventHandler(this.btnPersona_Click);
             // 
@@ -70,12 +63,11 @@
             this.btnBoleto.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnBoleto.Image = ((System.Drawing.Image)(resources.GetObject("btnBoleto.Image")));
             this.btnBoleto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBoleto.Location = new System.Drawing.Point(205, 272);
+            this.btnBoleto.Location = new System.Drawing.Point(659, 343);
             this.btnBoleto.Name = "btnBoleto";
-            this.btnBoleto.Size = new System.Drawing.Size(75, 23);
+            this.btnBoleto.Size = new System.Drawing.Size(125, 33);
             this.btnBoleto.TabIndex = 1;
             this.btnBoleto.Text = "Boleto";
-            this.btnBoleto.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnBoleto.UseVisualStyleBackColor = true;
             this.btnBoleto.Click += new System.EventHandler(this.btnBoleto_Click);
             // 
@@ -84,7 +76,7 @@
             this.btnMantenimiento.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnMantenimiento.Image = ((System.Drawing.Image)(resources.GetObject("btnMantenimiento.Image")));
             this.btnMantenimiento.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMantenimiento.Location = new System.Drawing.Point(205, 318);
+            this.btnMantenimiento.Location = new System.Drawing.Point(183, 344);
             this.btnMantenimiento.Name = "btnMantenimiento";
             this.btnMantenimiento.Size = new System.Drawing.Size(125, 32);
             this.btnMantenimiento.TabIndex = 2;
@@ -98,12 +90,11 @@
             this.btnEmpleado.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnEmpleado.Image = ((System.Drawing.Image)(resources.GetObject("btnEmpleado.Image")));
             this.btnEmpleado.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEmpleado.Location = new System.Drawing.Point(205, 175);
+            this.btnEmpleado.Location = new System.Drawing.Point(183, 201);
             this.btnEmpleado.Name = "btnEmpleado";
-            this.btnEmpleado.Size = new System.Drawing.Size(113, 23);
+            this.btnEmpleado.Size = new System.Drawing.Size(125, 33);
             this.btnEmpleado.TabIndex = 3;
             this.btnEmpleado.Text = "Empleado";
-            this.btnEmpleado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEmpleado.UseVisualStyleBackColor = true;
             this.btnEmpleado.Click += new System.EventHandler(this.btnEmpleado_Click);
             // 
@@ -112,12 +103,11 @@
             this.btnEquipaje.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnEquipaje.Image = ((System.Drawing.Image)(resources.GetObject("btnEquipaje.Image")));
             this.btnEquipaje.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEquipaje.Location = new System.Drawing.Point(745, 175);
+            this.btnEquipaje.Location = new System.Drawing.Point(659, 273);
             this.btnEquipaje.Name = "btnEquipaje";
-            this.btnEquipaje.Size = new System.Drawing.Size(88, 33);
+            this.btnEquipaje.Size = new System.Drawing.Size(125, 33);
             this.btnEquipaje.TabIndex = 4;
             this.btnEquipaje.Text = "Equipaje";
-            this.btnEquipaje.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEquipaje.UseVisualStyleBackColor = true;
             this.btnEquipaje.Click += new System.EventHandler(this.btnEquipaje_Click);
             // 
@@ -126,110 +116,24 @@
             this.btnEmbarque.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnEmbarque.Image = ((System.Drawing.Image)(resources.GetObject("btnEmbarque.Image")));
             this.btnEmbarque.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEmbarque.Location = new System.Drawing.Point(745, 221);
+            this.btnEmbarque.Location = new System.Drawing.Point(183, 273);
             this.btnEmbarque.Name = "btnEmbarque";
-            this.btnEmbarque.Size = new System.Drawing.Size(95, 29);
+            this.btnEmbarque.Size = new System.Drawing.Size(125, 33);
             this.btnEmbarque.TabIndex = 5;
             this.btnEmbarque.Text = "Embarque";
-            this.btnEmbarque.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEmbarque.UseVisualStyleBackColor = true;
             this.btnEmbarque.Click += new System.EventHandler(this.btnEmbarque_Click);
-            // 
-            // btnSeguridad
-            // 
-            this.btnSeguridad.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnSeguridad.Image = ((System.Drawing.Image)(resources.GetObject("btnSeguridad.Image")));
-            this.btnSeguridad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSeguridad.Location = new System.Drawing.Point(745, 318);
-            this.btnSeguridad.Name = "btnSeguridad";
-            this.btnSeguridad.Size = new System.Drawing.Size(95, 28);
-            this.btnSeguridad.TabIndex = 7;
-            this.btnSeguridad.Text = "Seguridad";
-            this.btnSeguridad.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnSeguridad.UseVisualStyleBackColor = true;
-            this.btnSeguridad.Click += new System.EventHandler(this.btnSeguridad_Click);
-            // 
-            // btnLimpieza
-            // 
-            this.btnLimpieza.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnLimpieza.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpieza.Image")));
-            this.btnLimpieza.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLimpieza.Location = new System.Drawing.Point(745, 414);
-            this.btnLimpieza.Name = "btnLimpieza";
-            this.btnLimpieza.Size = new System.Drawing.Size(88, 27);
-            this.btnLimpieza.TabIndex = 8;
-            this.btnLimpieza.Text = "LImpieza";
-            this.btnLimpieza.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnLimpieza.UseVisualStyleBackColor = true;
-            this.btnLimpieza.Click += new System.EventHandler(this.btnLimpieza_Click);
             // 
             // lblMenu
             // 
             this.lblMenu.AutoSize = true;
             this.lblMenu.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.lblMenu.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblMenu.Location = new System.Drawing.Point(348, 69);
+            this.lblMenu.Location = new System.Drawing.Point(311, 96);
             this.lblMenu.Name = "lblMenu";
             this.lblMenu.Size = new System.Drawing.Size(352, 22);
             this.lblMenu.TabIndex = 9;
             this.lblMenu.Text = "Seleccionar una de las siguirntes opciones";
-            // 
-            // btnAerolinea
-            // 
-            this.btnAerolinea.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnAerolinea.Image = ((System.Drawing.Image)(resources.GetObject("btnAerolinea.Image")));
-            this.btnAerolinea.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAerolinea.Location = new System.Drawing.Point(205, 370);
-            this.btnAerolinea.Name = "btnAerolinea";
-            this.btnAerolinea.Size = new System.Drawing.Size(101, 23);
-            this.btnAerolinea.TabIndex = 10;
-            this.btnAerolinea.Text = "Aerolinea";
-            this.btnAerolinea.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnAerolinea.UseVisualStyleBackColor = true;
-            this.btnAerolinea.Click += new System.EventHandler(this.btnAerolinea_Click);
-            // 
-            // btnCafeteria
-            // 
-            this.btnCafeteria.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnCafeteria.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnCafeteria.Image = ((System.Drawing.Image)(resources.GetObject("btnCafeteria.Image")));
-            this.btnCafeteria.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCafeteria.Location = new System.Drawing.Point(745, 365);
-            this.btnCafeteria.Name = "btnCafeteria";
-            this.btnCafeteria.Size = new System.Drawing.Size(88, 28);
-            this.btnCafeteria.TabIndex = 11;
-            this.btnCafeteria.Text = "Cafeteria";
-            this.btnCafeteria.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnCafeteria.UseVisualStyleBackColor = true;
-            this.btnCafeteria.Click += new System.EventHandler(this.btnCafeteria_Click);
-            // 
-            // btnSoporteTecnico
-            // 
-            this.btnSoporteTecnico.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnSoporteTecnico.Image = ((System.Drawing.Image)(resources.GetObject("btnSoporteTecnico.Image")));
-            this.btnSoporteTecnico.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSoporteTecnico.Location = new System.Drawing.Point(205, 410);
-            this.btnSoporteTecnico.Name = "btnSoporteTecnico";
-            this.btnSoporteTecnico.Size = new System.Drawing.Size(125, 31);
-            this.btnSoporteTecnico.TabIndex = 12;
-            this.btnSoporteTecnico.Text = "Soporte Tecnico";
-            this.btnSoporteTecnico.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnSoporteTecnico.UseVisualStyleBackColor = true;
-            this.btnSoporteTecnico.Click += new System.EventHandler(this.btnSoporteTecnico_Click);
-            // 
-            // btnMigracion
-            // 
-            this.btnMigracion.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnMigracion.Image = ((System.Drawing.Image)(resources.GetObject("btnMigracion.Image")));
-            this.btnMigracion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMigracion.Location = new System.Drawing.Point(745, 272);
-            this.btnMigracion.Name = "btnMigracion";
-            this.btnMigracion.Size = new System.Drawing.Size(95, 23);
-            this.btnMigracion.TabIndex = 6;
-            this.btnMigracion.Text = "Migracion";
-            this.btnMigracion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnMigracion.UseVisualStyleBackColor = true;
-            this.btnMigracion.Click += new System.EventHandler(this.btnMigracion_Click);
             // 
             // menuStrip1
             // 
@@ -238,7 +142,7 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1001, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(967, 24);
             this.menuStrip1.TabIndex = 14;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -285,15 +189,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1001, 543);
+            this.ClientSize = new System.Drawing.Size(967, 543);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.btnSoporteTecnico);
-            this.Controls.Add(this.btnCafeteria);
-            this.Controls.Add(this.btnAerolinea);
             this.Controls.Add(this.lblMenu);
-            this.Controls.Add(this.btnLimpieza);
-            this.Controls.Add(this.btnSeguridad);
-            this.Controls.Add(this.btnMigracion);
             this.Controls.Add(this.btnEmbarque);
             this.Controls.Add(this.btnEquipaje);
             this.Controls.Add(this.btnEmpleado);
@@ -318,13 +216,7 @@
         private Button btnEmpleado;
         private Button btnEquipaje;
         private Button btnEmbarque;
-        private Button btnSeguridad;
-        private Button btnLimpieza;
         private Label lblMenu;
-        private Button btnAerolinea;
-        private Button btnCafeteria;
-        private Button btnSoporteTecnico;
-        private Button btnMigracion;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem menuPrincipalToolStripMenuItem;
         private ToolStripMenuItem inicioToolStripMenuItem;
